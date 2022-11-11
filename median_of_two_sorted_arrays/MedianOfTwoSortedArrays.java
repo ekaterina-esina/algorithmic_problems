aa279553f6b4160e9242c706466a6ee35b3a0424
Esina Ekaterina <esinaekperm@gmail.com>
2022-11-11 17:36:06 +0500

    ALG-4 solution for algorithm MedianOfTwoSortedArrays

--- median_of_two_sorted_arrays/MedianOfTwoSortedArrays.java
+++ median_of_two_sorted_arrays/MedianOfTwoSortedArrays.java
@@ -1,14 +1,62 @@
+import java.util.Arrays;
 
 class MedianOfTwoSortedArrays {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int split1 = 0, split2 = 0;
 
+        if (nums1.length > nums2.length)
+            return findMedianSortedArrays(nums2, nums1);
+        int arrayLength = nums1.length + nums2.length;
+        int halfLength = arrayLength / 2;
+        int sptitL = 0, splitR = nums1.length;
+
+        while (sptitL <= splitR) {
+            split1 = (splitR - sptitL) / 2 + sptitL;
+            split2 = halfLength - split1;
+
+            double r1 = (split1 == nums1.length) ? Integer.MAX_VALUE : nums1[split1];
+            double l1 = (split1 == 0) ? Integer.MIN_VALUE : nums1[split1 - 1];
+
+            double r2 = (split2 == nums2.length) ? Integer.MAX_VALUE : nums2[split2];
+            double l2 = (split2 == 0) ? Integer.MIN_VALUE : nums2[split2 - 1];
+
+            if (r1 < l2)
+                sptitL = split1 + 1;
+            else if (l1 > r2)
+                splitR = split1 - 1;
+            else {
+                if (arrayLength % 2 == 0)
+                    return (Math.min(r1, r2) + Math.max(l1, l2)) / 2;
+                else
+                    return Math.min(r1, r2);
+            }
+        }
+        return -1;
     }
 }
-
 
 class Main {
     public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Invalid number of args");
+        } else {
+            int[] nums1 = parseStringToArray(0, args);
+            int[] nums2 = parseStringToArray(1, args);
+            double result = new MedianOfTwoSortedArrays().findMedianSortedArrays(nums1, nums2);
+            System.out.println(Double.toString(result));
         }
     }
+
+    private static int[] parseStringToArray(int index, String[] array){
+        String str = array[index].replaceAll("\\s+", "");
+        String[] strArray = str.split(",");
+        int[] nums = new int[strArray.length];
+        for (int i = 0; i < strArray.length; i++) {
+            nums[i] = Integer.parseInt(strArray[i]);
+        }
+        return nums;
+    }
 }
+
+
 
--- median_of_two_sorted_arrays/README.md
+++ median_of_two_sorted_arrays/README.md
@@ -28,9 +28,25 @@ Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
 
 
 ## Solution
+We have two sorted arrays, to find the median we need to sort all the values in both arrays.
+To do this, you need to get the number of all elements of the array and divide by 2.
+
+To move elements, we use binary search, which gives algorithmic complexity **O(log(max(m, n))**
+
+Now the first array will contain all numbers less than the second one. Further, if the number of elements is odd, then the result will be the element with the index number of elements / 2.
+If even, then add the largest number of the first array with the smallest number of the second array and divide by 2.
 
 ## Performance
 Run on JDK 11
+
+| **nums1 [ ]** | **nums2 [ ]** | **result** | **excecution time** |
+|:----------------:|:-------------:|:----------:|:-------------------:|
+|        [1,5,7,15]       |      [ ]      |    5.0     |      0.00031s       |
+|        [1,5,7,15]       |  [2,4,14,17]  |    6.0     |      0.00034s       |
+|       [1,2,4,5]        |     [2,3,7]      |    3.0     |      0.00034s       |
+|       [1,10,23,45,78,95, 101, 112,114,115,118,121,136,187,189,190,211]       |     [21,36,57,78,81,100, 111, 122,124,125,138,141,146,157,159,195,220]      |   116.5    |      0.00035s       |
+
+
 
 
 ## How to start
@@ -39,8 +55,7 @@ Install JDK 11
 Run in directory for compilation and start
 ```
 javac MedianOfTwoSortedArrays.java
-java Main
+java Main {first array} {second array}
+
+Params example: 1,5,7,15 2,4,14,17
 ```
-
-
-## Test data
